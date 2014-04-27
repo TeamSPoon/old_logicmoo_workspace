5e9bafcb6f866041f14212ec3b7afb89e34e96e6
Douglas R. Miles <logicmoo@gmail.com>
2014-04-26 23:12:10 -0700

    Create get_set.pl

---
+++ src_incoming/actions/get_set.pl
@@ -0,0 +1,16 @@
+:- module(get_set, []).
+/** <module> Agent changes one of their own variables
+% Douglas Miles 2014
+
+*/
+:- include(logicmoo('vworld/vworld_header.pl')).
+
+:- register_module_type(command).
+
+moo:decl_action('@get'(optional(term,self),atom,term),text("@get term to a property")).
+moo:decl_action('@set'(optional(term,self),atom,term),text("@sets term to a property")).
+
+moo:agent_call_command(Agent,'@set'(Obj,Prop,Value)) :- add(k(Prop,Obj,Value)).
+moo:agent_call_command(Agent,'@get'(Obj,Prop,Value)) :- req(k(Prop,Obj,Value)).
+
+:- include(logicmoo('vworld/vworld_footer.pl')).
