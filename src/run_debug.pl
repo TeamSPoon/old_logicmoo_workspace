3afcbc3b1992b23ec9b30fa2748bf14e8791e704
DouglasRMiles <miles.douglas.r@gmail.com>
2014-06-15 14:58:20 -0700

    accepts the missing file

--- src/run_debug.pl
+++ src/run_debug.pl
@@ -16,14 +16,18 @@ was_run_dbg_pl:-is_startup_file('run_debug.pl').
 % [Optionaly] re-define load_default_game
 % load_default_game:- load_game(logicmoo('rooms/startrek.all.pl')).
 
+% [Optionaly] load and start sparql server
+:- at_start(slow_work).
+% :- at_start(start_servers).
 
 start_boxer:-
+   threads,
    include(logicmoo(candc/parser_boxer)),
    fmt("Press Ctrl-D to start the mud!"),
    at_start(prolog).
 
-:- at_start(with_assertions(moo:prevent_transform_moo_preds,within_user(ignore(catch(start_boxer,_,true))))).
-
+% [Optional] This loads boxer
+% :- at_start(with_assertions(moo:prevent_transform_moo_preds,within_user(ignore(catch(start_boxer,_,true))))).
 
 % [Manditory] This loads the game and initializes so test can be ran
 :- if_flag_true(was_run_dbg_pl, at_start(run_setup)).
