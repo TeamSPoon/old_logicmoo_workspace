fba923c46fc6d43e1a6297a2664974a4f28167b8
DouglasRMiles <miles.douglas.r@gmail.com>
2014-07-09 20:24:09 -0700

    Wed Jul 9 20:24:06 PDT 2014

--- hg_push_pull.sh
+++ hg_push_pull.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-export OLDPWD="`pwd`"
-export NEWPWD="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
-
 
 while [ 1 ]
 do
-        cd $NEWPWD
-        git pull
-#        git update
-        cd $NEWPWD/wiki
-           git pull
-#           git update
-   echo "Hit CTRL+C $NEWPWD ${BASH_SOURCE[0]}";
-   cd $OLDPWD
-   sleep 20;
+ commitpush.sh `date`
+ sleep 10
 done
-
-cd $OLDPWD
