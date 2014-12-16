56735132bb014c28d0808babbe809bdf6a2d0930
DouglasRMiles <miles.douglas.r@gmail.com>
2014-12-15 21:53:14 -0800

    dirs

--- clone_externals.sh
+++ clone_externals.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/TeamSPoon/hMUD externals/hMUD
 git clone https://github.com/TeamSPoon/ClioPatria externals/ClioPatria
 git clone https://github.com/TeamSPoon/swish externals/swish
-echo Maybe: clone https://github.com/TeamSPoon/logicmoo externals/logicmoo
-git clone https://github.com/TeamSPoon/MUD_Examples games/
+git clone https://github.com/TeamSPoon/logicmoo externals/logicmoo
+git clone https://github.com/TeamSPoon/MUD_Examples examples/
+git clone https://github.com/TeamSPoon/XperiMental externals/XperiMental
 
