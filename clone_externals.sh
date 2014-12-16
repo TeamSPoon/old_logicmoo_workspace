68b345eba28dc783158255761b59e90c3d5d09ae
DouglasRMiles <miles.douglas.r@gmail.com>
2014-12-15 21:17:23 -0800

    clone_externals.sh

---
+++ clone_externals.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/TeamSPoon/hMUD externals/hMUD
+git clone https://github.com/TeamSPoon/ClioPatria externals/ClioPatria
+git clone https://github.com/TeamSPoon/swish externals/swish
+echo Maybe: clone https://github.com/TeamSPoon/logicmoo externals/logicmoo
+git clone https://github.com/TeamSPoon/MUD_Examples games/
+
