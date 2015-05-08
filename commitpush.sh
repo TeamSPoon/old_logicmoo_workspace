2ab98d3647d94a9b653914a5a11af339cb9c5d5f
DouglasRMiles <miles.douglas.r@gmail.com>
2015-05-08 16:36:00 -0700

    newscript

--- commitpush.sh
+++ commitpush.sh
@@ -1,11 +1,10 @@
-git pull
 git --git-dir=games/.git --work-tree=games pull
-
+git pull
 #% hg pull
 #% hg commit -m "$*"
-git commit -a -m "$*"
-git push
 git --git-dir=games/.git --work-tree=games commit
 git --git-dir=games/.git --work-tree=games push
+git commit -a -m "$*"
+git push
 #% hg push
 
