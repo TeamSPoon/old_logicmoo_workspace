25237d1f845f134f5809c25152f3064b36d87e5b
DouglasRMiles <miles.douglas.r@gmail.com>
2015-05-08 16:32:28 -0700

    Fri May 8 16:32:28 PDT 2015

--- commitpush.sh
+++ commitpush.sh
@@ -4,6 +4,7 @@ git --git-dir=games/.git --work-tree=games pull
 #% hg pull
 #% hg commit -m "$*"
 git commit -a -m "$*"
-#git push
+git --git-dir=games/.git --work-tree=games commit
+git push
 #% hg push
 
