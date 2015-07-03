dd380ff2164fdc2f40ff1efb0c38e25cd1f68347
Douglas R. Miles <logicmoo@gmail.com>
2015-07-03 02:43:49 -0700

    Create web_install.sh

---
+++ scripts/web_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
+
+git clone --recursive  https://github.com/TeamSPoon/PrologMUD 
+cd PrologMUD
+git fetch --recurse-submodules
+git submodule init
+git submodule update
+
