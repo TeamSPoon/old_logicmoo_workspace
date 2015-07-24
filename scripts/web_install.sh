e4165e24cab13e305c77ddf70bb950bd39bc36f3
logicmoo <logicmoo@gmail.com>
2015-07-24 05:43:00 -0500

    preconfig.sh better

--- scripts/web_install.sh
+++ scripts/web_install.sh
@@ -2,14 +2,16 @@
 
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
 
+mkdir -p /opt
 cd /opt
 git clone --recursive  https://github.com/TeamSPoon/PrologMUD 
 cd PrologMUD
+git pull
 git fetch --recurse-submodules
 git submodule init
 git submodule update
+git submodule sync --recursive
 
-git pull
 
 ( source ./scripts/preconfig.sh )
 
