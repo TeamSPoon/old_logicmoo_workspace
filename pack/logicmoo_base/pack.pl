834b8f7676f053cca6a552321f3c9028d614eae3
logicmoo <logicmoo@gmail.com>
2015-10-03 22:00:44 -0700

    Closer to Logicmoo_base Pack

--- pack/logicmoo_base/pack.pl
+++ pack/logicmoo_base/pack.pl
@@ -1,8 +1,8 @@
 name(logicmoo_base).
-version('0.0.4').
+version('0.0.5').
 author('Douglas R. Miles','logicmoo@gmail.com').
 title('Support for various Prolog programing extensions such reactive [agent] programming using forward-chaining').
 keywords([fc,reactive,dynamic,runtime,fol,snark,logicmoo]).
 home('https://github.com/TeamSPoon/PrologMUD/tree/master/pack/logicmoo_base').
-download('http://www.prologmoo.com/packs/logicmoo_base/release/logicmoo_base-0.0.4.tgz').
+download('http://www.prologmoo.com/packs/logicmoo_base/release/logicmoo_base-0.0.5.tgz').
 
