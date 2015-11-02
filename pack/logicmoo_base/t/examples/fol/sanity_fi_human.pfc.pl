d17e8e0a4698bc75d25a3355b408bd2fd2a56cc4
logicmoo <logicmoo@gmail.com>
2015-11-02 11:00:09 -0800

    though fol/sanity_fi_human fails this is the entry to new skolem represention

--- pack/logicmoo_base/t/examples/fol/sanity_fi_human.pfc.pl
+++ pack/logicmoo_base/t/examples/fol/sanity_fi_human.pfc.pl
@@ -1,63 +1,18 @@
 #!/usr/bin/env swipl
 
-:- module(sanity_fi,[]).
+:- include('sanity_fi_sk.pfc').
 
-
-:-
- op(1199,fx,('==>')), 
- op(1190,xfx,('::::')),
- op(1180,xfx,('==>')),
- op(1170,xfx,'<==>'),  
- op(1160,xfx,('<-')),
- op(1150,xfx,'=>'),
- op(1140,xfx,'<='),
- op(1130,xfx,'<=>'), 
- op(600,yfx,'&'), 
- op(600,yfx,'v'),
- op(350,xfx,'xor'),
- op(300,fx,'~'),
- op(300,fx,'-').
-
-must_is_entailed(G):- cwc, must(is_entailed(G)).
-show_test(G):- cwc, get_user_abox(KB),printAll(must(KB:G)).
-
-:- op(1100,fx,(shared_multifile)).
-
-% :- include('test_header.pfc').
-:- use_module(library(logicmoo/logicmoo_user)).
-
-:- set_user_abox(baseKB).
-%=  setup pfc
-:- set_current_module(baseKB).
-:- begin_pfc.
-:- sanity(get_user_abox(baseKB)).
-
-:- process_this_script.
-
-%= save compiled clauses using forward chaining storage (by default)
-%= we are using forward chaining just so any logical errors, performance and program bugs manefest
-%= immediately
-:- set_clause_compile(fwc).
-
-%= ````
-%= logic tests...
-%= ````
-%:- debug(mpred).
-
-%= trudy is human
-human(trudy).
-human(eileen).
-human(douglas).
-mother(trudy,eileen).
-mother(eileen,douglas).
+% simply retract (so we can deduce)
+\+ human(douglas).
 
 %= catch a regression bug that may couse trudy to lose human assertion
 never_retract_u(human(trudy)).
 
+related_to(P1,P2)=>related_to(P2,P1).
 
-((human(P1),father(P1,P2))=>human(P2)).
-((human(P1),mother(P1,P2))=>human(P2)).
-forall(c,exists([m,f], if(human(c), (mother(m,c) & father(f,c))))).
+father(P1,P2)=>related_to(P1,P2).
+
+%human(P1),related_to(P1,P2)=>human(P2).
 
 :- printAll(must(father(_,_))).
 :- printAll(must(mother(_,_))).
