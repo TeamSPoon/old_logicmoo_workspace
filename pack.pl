91f90d492f04d182063434b6965866ae84f01db8
logicmoo <logicmoo@gmail.com>
2016-04-24 03:25:29 -0700

    making prologmud puck live nder its pack dir

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
-name(prologmud).
+name(prologmud_scm).
 version('1.1.1').
 author('Douglas R. Miles','logicmoo@gmail.com').
-title('Online text adventure game').
+title('Logicmoo MUD SCM Pack that holds all sub packs in one directory').
 keywords([mud,mpred,games,ai,logicmoo]).
 home('http://prologmoo.com/packs').
 requires('logicmoo_base').
-download('https://packs.prologmoo.com/packs/prologmud/release/prologmud-1.1.1.tgz').
+download('https://packs.prologmoo.com/packs/prologmud/release/prologmud_scm-1.1.1.tgz').
 
