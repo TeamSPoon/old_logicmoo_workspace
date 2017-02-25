1fbde3710fbf6b1a925dab56028c0a1cfc54883f
root <logicmoo@gmail.com>
2017-02-25 07:52:23 -0800

    linefeed madness

--- pack/prologmud/pack.pl
+++
@@ -1,9 +0,0 @@
-name(prologmud).
-version('1.1.1').
-author('Douglas R. Miles','logicmoo@gmail.com').
-title('Online text adventure game').
-keywords([mud,mpred,games,ai,logicmoo]).
-home('http://prologmoo.com/packs').
-requires('logicmoo_base').
-download('https://packs.prologmoo.com/packs/prologmud/release/prologmud-1.1.1.tgz').
-
--- pack/prologmud/prolog/prologmud/mobs/mobs_conf.pl
+++
@@ -1,9 +0,0 @@
-/* * module * 
-% This module is the CircleMUD configurator
-*/
-
-:-swi_module(mobs_conf,[]).
-
-:- locally(t_l:pfcExpansion,ensure_loaded(affordance/simbots)).
-
-% action_info_db
