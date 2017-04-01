f7673c9da1270a9eed6c0fbb53575f7fead438b9
root <logicmoo@gmail.com>
2017-03-31 19:17:44 -0700

    rdf_rewrite:arity/2 2017-03-31-19:16:51

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(logicmoo_devel).
 title('The https://github.com/TeamSPoon/PrologMUD.git development tree that is hosting several packs at once').
 
-version('1.1.117').
+version('1.1.118').
 download('https://github.com/TeamSPoon/PrologMUD/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
